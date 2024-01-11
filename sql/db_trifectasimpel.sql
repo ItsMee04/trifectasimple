88eb26867d5557687f2e346326e06a52b604b46b
ItsMee04 <meemeeits@gmail.com>
2024-01-12 06:29:13 +0700

    update Database

--- sql/db_trifectasimpel.sql
+++ sql/db_trifectasimpel.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2024 at 02:56 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 12, 2024 at 12:28 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,7 +44,7 @@ INSERT INTO `identitas` (`id`, `jenisidentitas`, `status`) VALUES
 (5, 'PASSPORT', 1),
 (6, 'KARTU IZIN TINGGAL SEMENTARA (KITAS)', 1),
 (7, 'KARTU IZIN TINGGAL TETAP (KITAP)', 1),
-(8, 'KTP WNA', 1);
+(10, 'KTP WNA', 1);
 
 -- --------------------------------------------------------
 
@@ -87,8 +87,8 @@ CREATE TABLE `karyawan` (
 --
 
 INSERT INTO `karyawan` (`id`, `nama`, `alamat`, `kontak`, `profesi`, `ttd`, `status`) VALUES
-(1, 'Indra Kusuma', 'Purwokerto', '0822-1133-1113', 1, '2024-1704883590.png', 1),
-(2, 'Dimas Anugerah', 'Purbalingga', '0822-1133-1113', 1, '2024-1704892024.png', 1);
+(1, 'Indra Kusuma', 'Purwokerto', '0822-1133-1113', 1, '2024-1704901076.png', 1),
+(3, 'Dimas Anugerah', 'Purbalingga', '0822-1133-1113', 2, '', 1);
 
 -- --------------------------------------------------------
 
@@ -167,6 +167,26 @@ INSERT INTO `profesi` (`id`, `jenisprofesi`, `status`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(55) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `role`, `status`) VALUES
+(1, 'Admin', 1),
+(2, 'User', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `status`
 --
 
@@ -217,7 +237,7 @@ CREATE TABLE `users` (
   `username` varchar(55) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL,
-  `status` int(11) NOT NULL
+  `status` int(11) NOT NULL DEFAULT 2
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -225,7 +245,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `iduser`, `username`, `password`, `role`, `status`) VALUES
-(1, 1, 'indrakusuma', '$2y$10$6qMYqRsCDlxviOdhGVgrKuKfhmkxf306QZxzs1YwweoenR.24eNA2', 1, 1);
+(1, 1, 'indrakusuma', '$2y$12$.Qw/y33owwFeOPBYKLUxOOB1ESywtNqHUxNskTzhKYw/NrdSAXoIC', 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -268,6 +288,12 @@ ALTER TABLE `profesi`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `status`
 --
 ALTER TABLE `status`
@@ -293,7 +319,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `identitas`
 --
 ALTER TABLE `identitas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `jenisproduk`
@@ -305,7 +331,7 @@ ALTER TABLE `jenisproduk`
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `kontak`
@@ -326,6 +352,12 @@ ALTER TABLE `profesi`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `status`
 --
 ALTER TABLE `status`
@@ -341,7 +373,7 @@ ALTER TABLE `suplier`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
